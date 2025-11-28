7bd14d7ae14095510b5163948cb25234b48e0ce4
brandon-hog <brandon.m.hoggatt@gmail.com>
2025-11-28 15:34:55 -0600

    Adding repositories.

---
+++ src/main/java/com/brandonmh/library/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.brandonmh.library.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.brandonmh.library.model.Book;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+}
---
+++ src/main/java/com/brandonmh/library/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.brandonmh.library.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.brandonmh.library.model.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
